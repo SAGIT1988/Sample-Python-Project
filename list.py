36bbc470ae1f3b8729080610667586aace9c2407
SAGIT1988 <sujatha.a19@gmail.com>
2022-08-11 14:41:52 -0500

    new file

---
+++ list.py
@@ -0,0 +1,20 @@
+
+List1=[8,5,9,3,10]
+List1.reverse()
+print ("reversed list is",List1)
+
+List2=[8,15,3,5,1,9,13,4,10]
+List2.sort()
+print("The sorted list is",List2)
+
+List3= []
+List3=l=List1.copy()
+print("List 3",List3)
+
+indexvalue = List2[2:6]
+print("The index values are:",indexvalue)
+
+a=10
+b=15
+c=a+b
+print("addtion is ",c)
